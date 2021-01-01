c133b298777678954904a3b7ac2f296057232bf8
Will Diep <widiep@gmail.com>
2021-01-01 01:54:22 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchCats } from './actions/catActions'
+import CatList from './CatList'
 
-class App extends Component {   
-  
+class App extends Component {
+  componentDidMount() {
+    console.log(this.props)
+    this.props.fetchCats()
+  }
+
+  handleLoading = () => {
+    console.log(this.props.loading)
+    if (this.props.loading) {
+      return <div>Loading...</div>
+    } else {
+      return <CatList catPics={this.props.catPics} />
+    }
+  }
+
   render() {
+    console.log(this.props.catPics) // log will fire every time App renders
     return (
-      <div>
+      <div className='App'>
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        {this.handleLoading()}
       </div>
-    );
+    )
   }
 }
 
-export default App
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading,
+  }
+}
 
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCats: () => dispatch(fetchCats()),
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,28 @@
-// write your CatList component here
+import React from 'react'
+
+// function CatList({ catPics }) {
+//   console.log('from CatList')
+//   return catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
+// }
+
+class CatList extends React.Component {
+  // listCats = () => {
+  //   return this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
+  // }
+
+  
+  render() {
+    const listCats = this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
+
+    console.log('from CatList')
+    
+    return (
+      <div>
+        {/* {this.listCats()} */}
+        {listCats}
+      </div>
+    )
+  }
+}
+
+export default CatList
