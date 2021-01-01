6a17e1dd377db2f6ed95536d51e95076a9273c14
Will Diep <widiep@gmail.com>
2021-01-01 12:43:11 -0800

    commit tree eeaac6b11ce018770c4e1b3609cba679c2eaa846

--- src/CatList.js
+++ src/CatList.js
@@ -1,28 +1,26 @@
 import React from 'react'
 
-// function CatList({ catPics }) {
-//   console.log('from CatList')
-//   return catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
-// }
-
-class CatList extends React.Component {
-  // listCats = () => {
-  //   return this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
-  // }
-
-  
-  render() {
-    const listCats = this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
-
-    console.log('from CatList')
-    
-    return (
-      <div>
-        {/* {this.listCats()} */}
-        {listCats}
-      </div>
-    )
-  }
+function CatList({ catPics }) {
+  return catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
 }
+
+// class CatList extends React.Component {
+//   listCats = () => {
+//     return this.props.catPics.map(cat => (
+//       <img key={cat.id} src={cat.url} alt={cat.id} />
+//     ))
+//   }
+
+//   render() {
+//     // const listCats = this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
+
+//     return (
+//       <div>
+//         {this.listCats()}
+//         {/* {listCats} */}
+//       </div>
+//     )
+//   }
+// }
 
 export default CatList
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
  
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import catsReducer from './reducers/catsReducer';
  
-const store = createStore(catsReducer, applyMiddleware(thunk))
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(catsReducer, composeEnhancers(applyMiddleware(thunk)))
 
 // console.log(store.getState())
  
